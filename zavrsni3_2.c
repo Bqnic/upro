2e180257da87ed107f8e55cca23bfa700fcbfdf8
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-01-20 19:43:14 +0100

    Update zavrsni3_2.c

--- zavrsni3_2.c
+++ zavrsni3_2.c
@@ -1,11 +1,10 @@
 void SumaVektora(struct vektor_s *pok, int n, struct vektor_s *rez){
 
-    rez->komp_i = 0;
-    rez->komp_j = 0;
-    
+   rez->komp_i = 0;
+   rez->komp_j = 0;
+   
     for (int i = 0; i < n; i++){
-        
-        rez[i].komp_i += pok[i].komp_i;
-        rez[i].komp_j += pok[i].komp_j;
+        rez->komp_i += pok[i].komp_i;
+        rez->komp_j += pok[i].komp_j;
     }
 }
