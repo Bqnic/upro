6815f84e784b87b8207466b71ff61d7c3e958421
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-01-20 19:45:29 +0100

    Add files via upload

---
+++ zavrsni3_4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+_Bool txt2bin(char *inputFile, char *outputFile){
+
+    FILE *text = fopen(inputFile, "r");
+    FILE *bin = fopen(outputFile, "wb");
+
+    if (text == NULL || bin == NULL)
+        return 0;
+
+    struct record kod;
+
+    while(fscanf(text, "%2d%3d%3d", &kod.code, &kod.x, &kod.y) == 3){
+        fwrite(&kod, sizeof(kod), 1, bin);
+    }
+
+    return 1;
+    
+}
